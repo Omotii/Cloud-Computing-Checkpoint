ca07bafd0f90840b826e2824cb0e9f7adf658843
Omotii <martinoba01@gmail.com>
2023-12-20 18:55:53 +0100

    cloud-google-map

---
+++ src/component/body.jsx
@@ -0,0 +1,22 @@
+import './styles.css'
+import Map from './map'
+
+const Body = () => {
+    return (
+        <>
+            <header>
+                <div className='logo-img-container'>
+                    <img src='/images/coffe.jpg' alt='coffee'/>
+                </div>
+                <h1>Lagos Coffee Shop</h1>
+                <p>Just brewed happiness in a cup!</p>
+            </header>
+            <main>
+                <p>On your next trip, locate us at Chevron Toll Plaza, Chevron Drive, Lekki, Lagos State</p>
+                <Map/>
+            </main>
+        </>
+    )
+}
+
+export default Body
---
+++ src/component/map.jsx
@@ -0,0 +1,12 @@
+import './styles.css'
+
+const Map = () => {
+    return (
+        <>
+            <iframe width="950" height="360" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=879&amp;height=403&amp;hl=en&amp;q=Chevron%20Toll%20Plaza,%20Lekki,%20Lagos%20Ilford+(Coffee%20Shop)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" title="Our Coffee Shop"></iframe>
+
+        </>
+    )
+}
+
+export default Map
